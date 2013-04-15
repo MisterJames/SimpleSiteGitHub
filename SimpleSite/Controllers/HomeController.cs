9e2a39687fbca886f82f287d21e6ea76b2ae08fe
James Chambers <james@jameschambers.com>
2013-04-14 22:06:17 -0500

    initialcommit

---
+++ SimpleSite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Angela.Core;
+using SimpleSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Person> _people = Angie.FastList<Person>();
+
+        public ActionResult Index()
+        {            
+            return View(_people);
+        }
+
+    }
+}
